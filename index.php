71ecbce0aee16303ca1da51a91f967c2088bfcd3
rutaDarbas <drungilaite.r@gmail.com>
2017-06-07 21:12:19 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fotografo portfolio</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<meta charset="utf-8">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+
+<header>
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">
+			        <img alt="Brand" src="img/rsz_1rsz_photo-logo.jpg">
+		      </a>
+		    </div>
+		  </div>
+		</nav>
+
+		<nav>
+			<ol class="breadcrumb">
+			  <li><a href="#">Home</a></li>
+			  <li><a href="#">Photography guide</a></li>
+			  <li><a href="#" class="active">Galery</a></li>
+			  <li><a href="#" class="active">About us</a></li>
+			</ol>	
+		</nav>
+</header>
+
+<body>
+
+	
+
+
+
+
+
+	<footer class="page-footer">
+            <div class="container text-wide">
+              <div class="row">
+                <div class="col l6 s12">
+                  <h5 class="white-text"> 
+                  <ul class="footer-contacts">
+                  <li>Adresas: Liudviko Zamenhofo g. 6,</li>
+                   <li>Fabioniškės</li>
+                  <li>LT-08236, Vilnius</li>
+                  <li>drungilaite.r@gmail.com</li>
+                  <li>+370 621 16441</li>
+                  </ul>
+                  <p class="grey-text text-lighten-4"></p>
+                </div>
+                <div class="col l6 s12 right-align">
+                <ul class="footer-kontaktai">
+                  <li><h5 class="white-text partner-text">Remėjai:</h5></li>
+        		</ul>    
+                </div>
+              </div>
+            </div>
+            <div class="footer-copyright">
+              <div class="container">
+              © 2017 Copyright R8studio
+              <a class="grey-text text-lighten-4 right" href="#!"></a>
+              </div>
+            </div>
+          </footer>
+          </body>
+</html>
